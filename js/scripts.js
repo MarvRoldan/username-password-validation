e63b24c4f60a3d33c1aac61e7d97b635f43cf1b6
MarvRoldan <marvinroldan_@hotmail.com>
2020-03-29 17:55:21 -0600

    Added onclick to button

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,40 @@
+// -- Regex cheat sheet: https://www.rexegg.com/regex-quickstart.html --
 const patterns = {
     username: /^[\w]{5,}$/i,
-    password: /^[\w!@#\$%\^&\*]{8,}/
+    password: /^[\w!@#\$%\^&\*]{8,}/i
 };
+const inputs = document.querySelectorAll( "input" );
+const userName = document.getElementById( "userName" );
+const password = document.getElementById( "password" );
+const signUpButton = document.getElementById ( "signUpButton" ); 
 
-const inputs = document.querySelectorAll( 'input' );
 
+
+// -- Listens to user inputs --
 inputs.forEach(( input ) => {
-    input.addEventListener( 'keyup', ( e ) => {
+    input.addEventListener( "keyup", ( e ) => {
         validation( e.target, patterns[ e.target.attributes.name.value ] )
-
     });
 });
 
-// Validates user inputs
+// -- Validates user inputs --
 validation = (userinputs, regex) => {
    if( regex.test(userinputs.value) ){
-       userinputs.className = 'true';
+       userinputs.className = "true";
    } else {
-       userinputs.className = 'false';
+       userinputs.className = "false";
    }
-}
+};
+
+// -- Local Storage --
+signUpButton.onclick = () => {
+    const user = userName.value;
+    const pass = password.value;
+
+    console.log(user);
+    console.log(pass);
+};
+
+
+
+
